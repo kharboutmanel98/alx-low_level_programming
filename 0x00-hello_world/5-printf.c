d07b6f7e6d406235ce8e2e062d0ca673e94895c5
kharboutmanel98 <kharbout2017@gmail.com>
2023-02-16 15:44:16 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,8 @@
 /**
  * main Entry - point
  *
- * Description: 'the program's description'
- *
  * Return: 0 (success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
