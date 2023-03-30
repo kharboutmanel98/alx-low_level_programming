2e84ba8d572e0d7bdbb48d91484d960470f31b64
kharboutmanel98 <kharbout2017@gmail.com>
2023-03-30 16:15:14 -0700

    Write a function that prints all the elements of a list_t list.

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,15 @@
+#define <unistd.h>
+
+/**
+  * _putchar - prints character to stdout
+  *
+  * @c: character to print
+  *
+  * Return: 1 on success
+  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
