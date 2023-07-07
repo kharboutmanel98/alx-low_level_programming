a6b56ef244a74605936d671d35089d6022eff912
kharboutmanel98 <kharbout2017@gmail.com>
2023-07-07 11:10:50 -0700

    commit

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
+ * Return: address of dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	a = b = 0;
+	while (*(dest + a))
+		a++;
+	while (b < n && *(src + b))
+	{
+		*(dest + a) = *(src + b);
+		a++;
+		b++;
+	}
+	if (b < n)
+		*(dest + a) = *(src + b);
+	return (dest);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function that print a string, followed by new line
+ * @str: pointer that point the string
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
